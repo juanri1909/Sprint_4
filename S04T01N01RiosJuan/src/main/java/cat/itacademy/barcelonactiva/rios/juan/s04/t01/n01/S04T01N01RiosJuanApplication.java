8d215a2afc2d632d1a98cdedeccaefcd5764ce2a
juanri1909 <jrigarci1909@gmail.com>
2022-04-10 23:49:39 +0200

    Add files via upload

---
+++ S04T01N01RiosJuan/src/main/java/cat/itacademy/barcelonactiva/rios/juan/s04/t01/n01/S04T01N01RiosJuanApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.rios.juan.s04.t01.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01RiosJuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01RiosJuanApplication.class, args);
+	}
+
+}
