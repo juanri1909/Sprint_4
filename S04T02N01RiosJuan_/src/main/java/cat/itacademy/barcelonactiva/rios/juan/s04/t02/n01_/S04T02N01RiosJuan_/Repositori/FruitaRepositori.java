064c63e58e82dde53e690fbeb14c0288b9334a6f
juanri1909 <jrigarci1909@gmail.com>
2022-04-20 01:10:51 +0200

    Delete S04T02N01RiosJuan_ directory

--- S04T02N01RiosJuan_/src/main/java/cat/itacademy/barcelonactiva/rios/juan/s04/t02/n01_/S04T02N01RiosJuan_/Repositori/FruitaRepositori.java
+++
@@ -1,8 +0,0 @@
-package cat.itacademy.barcelonactiva.rios.juan.s04.t02.n01_.S04T02N01RiosJuan_.Repositori;
-
-import org.springframework.data.repository.CrudRepository;
-import cat.itacademy.barcelonactiva.rios.juan.s04.t02.n01_.S04T02N01RiosJuan_.Entity.Fruita;
-
-
-public interface FruitaRepositori extends CrudRepository<Fruita, Integer>{
-}
